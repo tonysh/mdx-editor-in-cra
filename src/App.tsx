dd1a4c05cba05560c9566b8a3db4599e1aed0b9b
tony <tony@localhost>
2023-12-19 20:59:52 +0800

    The first plugin setting works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { MDXEditor } from '@mdxeditor/editor'
+import React from 'react'
+
+import { MDXEditor, MDXEditorMethods } from '@mdxeditor/editor'
+import { listsPlugin, headingsPlugin } from '@mdxeditor/editor'
 import '@mdxeditor/editor/style.css'
 
 function App() {
+  // construct a ref to the editor
+  const ref = React.useRef<MDXEditorMethods>(null)
   return (
-    <MDXEditor markdown='Hello world' />
+    <>
+      <button onClick={() => ref.current?.setMarkdown('new markdown')}>Set new markdown</button>
+      <button onClick={() => console.log(ref.current?.getMarkdown())}>Get markdown</button>
+      <MDXEditor ref={ref} markdown='*hello world*' plugins={[headingsPlugin(), listsPlugin()]} onChange={console.log} />
+    </>
   );
 }
 
