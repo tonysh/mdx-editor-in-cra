05a17883155bc5c1095183cf36a1ac3bfe3bbecb
tony <tony@localhost>
2023-12-19 21:14:29 +0800

    clipboard works

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { MDXEditor, MDXEditorMethods } from '@mdxeditor/editor'
 import { listsPlugin, headingsPlugin } from '@mdxeditor/editor'
 import '@mdxeditor/editor/style.css'
 
+function write_clipboard(text: string) {
+  navigator.clipboard.writeText(text);
+}
+
 function App() {
   // construct a ref to the editor
   const ref = React.useRef<MDXEditorMethods>(null)
@@ -11,7 +15,7 @@ function App() {
     <>
       <button onClick={() => ref.current?.setMarkdown('new markdown')}>Set new markdown</button>
       <button onClick={() => console.log(ref.current?.getMarkdown())}>Get markdown</button>
-      <MDXEditor ref={ref} markdown='*hello world*' plugins={[headingsPlugin(), listsPlugin()]} onChange={console.log} />
+      <MDXEditor ref={ref} markdown='*hello world*' plugins={[headingsPlugin(), listsPlugin()]} onChange={write_clipboard} />
     </>
   );
 }
