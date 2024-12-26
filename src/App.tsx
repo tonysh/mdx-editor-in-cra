4a343fea4791292079c423da0e2f23b9fe69efa5
tony <tony@localhost>
2024-12-26 10:13:39 +0800

    basic function works

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 
 import { MDXEditor, MDXEditorMethods } from '@mdxeditor/editor'
-import { listsPlugin, headingsPlugin, quotePlugin, linkPlugin, tablePlugin, toolbarPlugin, codeBlockPlugin } from '@mdxeditor/editor'
+import { listsPlugin, headingsPlugin, quotePlugin, linkPlugin, tablePlugin, toolbarPlugin, codeBlockPlugin, diffSourcePlugin, DiffSourceToggleWrapper, BoldItalicUnderlineToggles, UndoRedo } from '@mdxeditor/editor'
 import '@mdxeditor/editor/style.css'
 
 function write_clipboard(text: string) {
   navigator.clipboard.writeText(text);
-}
-
-async function load_clipboard() {
-  return await navigator.clipboard.readText();
 }
 
 function App() {
@@ -17,13 +13,16 @@ function App() {
   const ref = React.useRef<MDXEditorMethods>(null)
   return (
     <>
-      <button onClick={() => load_clipboard().then((c) => { ref.current?.setMarkdown(c) })}>Load</button>
       <button onClick={() => write_clipboard(ref.current!.getMarkdown())}>Copy</button>
-      <MDXEditor ref={ref} markdown='*hello world*' plugins={[
-            listsPlugin(), headingsPlugin(), quotePlugin(), linkPlugin(), tablePlugin(), toolbarPlugin(), codeBlockPlugin()
+      <MDXEditor ref={ref} markdown='' autoFocus placeholder='Write here' plugins={[
+            listsPlugin(), headingsPlugin(), quotePlugin(), linkPlugin(), tablePlugin(), codeBlockPlugin(), diffSourcePlugin(),
+            toolbarPlugin({
+                  toolbarContents: () => (<> <DiffSourceToggleWrapper> <></> </DiffSourceToggleWrapper> </>)
+            })
       ]} />
     </>
   );
 }
 
 export default App;
+
